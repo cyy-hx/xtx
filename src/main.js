a569487282f2e99f5a3ed3a5090e088cb321a03a
Your Name <your.email@example.com>
2025-09-26 14:11:41 +0800

    scss文件的自动导入

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入初始化样式文件
+import '@/styles/common.scss'
+
 import './assets/main.css'
 
 import { getCategory }  from '@/apis/testAPI'
