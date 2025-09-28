c80d8d199028436b91079514d35c31affce46e2b
Your Name <your.email@example.com>
2025-09-28 15:38:33 +0800

    二级分类-整体认识和路由配置

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,8 @@
 import { useBanner } from './composables/useBanner'
 import GoodsItem from '../Home/components/GoodsItem.vue'
 import { useCategory } from './composables/useCategory'
+
+
 const { bannerList } = useBanner()
 const { categoryData } = useCategory()
 //获取数据
@@ -33,7 +35,7 @@ const { categoryData } = useCategory()
           <h3>全部分类</h3>
           <ul>
             <li v-for="i in categoryData.children" :key="i.id">
-              <RouterLink to="/">
+              <RouterLink :to="`/category/sub/${i.id}`">
                 <img :src="i.picture" />
                 <p>{{ i.name }}</p>
               </RouterLink>
