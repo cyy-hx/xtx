69f0f1da39e6f4b07ce3276dd042d0b4088fd683
Your Name <your.email@example.com>
2025-11-03 19:11:58 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -10,7 +10,8 @@ import { ref } from 'vue';
 //1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 })
 
 //2.准备规则对象
@@ -21,6 +22,20 @@ const rules = {
   password:[
     {required:true,message:'密码不能为空',trigger:'blur'},
     {min:6,max:14,message:'密码长度在6-14之间',trigger:'blur'}
+  ],
+  agree:[
+    {
+      validator:(rule,value,callback)=>{
+        console.log(value);
+        //自定义校验逻辑
+        //勾选就通过，不勾选就不通过
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选同意隐私条款和服务条款'))
+        }
+      }
+    }
   ]
 
 }
@@ -56,8 +71,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
