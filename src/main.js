a49d520b15e368834684c7e9e8895dae5df9d89b
Your Name <your.email@example.com>
2025-11-03 14:11:48 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ import './assets/main.css'
 //引入懒加载指令插件并且注册
 import lazyPlugin from '@/directives'
 
+import {componentPlugin} from '@/components'
+
 
 
 import { ElInfiniteScroll } from 'element-plus'
@@ -24,6 +26,6 @@ app.directive('infinite-scroll', ElInfiniteScroll)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
-
+app.use(componentPlugin)
 app.mount('#app')
 
