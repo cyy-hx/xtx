aec6257b61ff9405aa2e0302e023d487ad8663ad
Your Name <your.email@example.com>
2025-11-06 20:19:33 +0800

    支付结果的显示

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 
 
 const router = createRouter({
@@ -45,6 +46,9 @@ const router = createRouter({
         },{
           path:'pay',
           component:Pay
+        },{
+          path:'paycallback',
+          component:PayBack
         }
       ]
     },
